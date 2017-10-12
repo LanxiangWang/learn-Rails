1ec76930856d31781b5bb7b540e5b623bbff97f3
Lanxiang Wang <lanxiang.wang13@gmail.com>
2017-10-11 21:25:53 -0500

    customized routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/home'
+  root to: 'pages#home'
 
-  get 'pages/about'
+  get 'about', to: 'pages#about'
 
-  get 'pages/contact'
+  get 'contact', to: 'pages#contact'
 
   resources :recipes
   resources :blogs
