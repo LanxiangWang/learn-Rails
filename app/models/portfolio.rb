e1602d7054dceb93deb229f9d4f894a0e311cf4c
Lanxiang Wang <lanxiang.wang13@gmail.com>
2017-10-22 12:56:34 -0500

    Implemented concern

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,10 +1,17 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
+
   validates :title, :body, :main_image, :thumb_image, presence: true
 
   after_initialize :set_defaults
 
   def set_defaults
-    self.main_image ||= "http://via.placeholder.com/600x400"
-    self.thumb_image ||= "http://via.placeholder.com/350x200"
+    self.main_image ||= Placeholder.image_generator('600', '250')
+    self.thumb_image ||= Placeholder.image_generator('250', '250')
   end
+
+  def self.angular
+    where(subtitle: 'Angular')
+  end
+
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,10 @@
 class Skill < ApplicationRecord
+  include Placeholder
   validates :title, :percent_utilized, presence: true
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.badge ||= Placeholder.image_generator(250, 250)
+  end
 end
